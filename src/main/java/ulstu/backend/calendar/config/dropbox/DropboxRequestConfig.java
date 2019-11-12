5a333c70b83f58aab74dadf68dfc5b8fa98d2e4f
trepixert <pixert@mail.ru>
2019-11-13 02:40:35 +0400

    hided token

--- src/main/java/ulstu/backend/calendar/config/dropbox/DropboxRequestConfig.java
+++ src/main/java/ulstu/backend/calendar/config/dropbox/DropboxRequestConfig.java
@@ -2,13 +2,15 @@ package ulstu.backend.calendar.config.dropbox;
 
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
-@Component
+@Configuration
+@EnableEncryptableProperties
 public class DropboxRequestConfig {
     @Value("${dropbox.access.token}")
     private String ACCESS_TOKEN;
