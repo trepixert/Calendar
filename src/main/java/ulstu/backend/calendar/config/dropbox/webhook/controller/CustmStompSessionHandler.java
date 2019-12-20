6f378f522e101bb9def736bbdbb06256bebb7a40
trepixert <pixert@mail.ru>
2019-12-20 05:17:27 +0400

    removed redundant files

--- src/main/java/ulstu/backend/calendar/config/dropbox/webhook/controller/CustmStompSessionHandler.java
+++ src/main/java/ulstu/backend/calendar/config/dropbox/webhook/controller/CustmStompSessionHandler.java
@@ -1,6 +1,32 @@
 package ulstu.backend.calendar.config.dropbox.webhook.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
+import java.lang.reflect.Type;
+
+@Log4j2
 public class CustmStompSessionHandler extends StompSessionHandlerAdapter {
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        exception.printStackTrace();
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+        exception.printStackTrace();
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return String.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        log.info("Received: {}", payload.toString());
+    }
 }
